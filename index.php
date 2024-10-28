ef5b0c985be8d9bec966941e641467859ab56376
sunbody1024 <sunbody1024@gmail.com>
2024-10-28 10:48:15 +0800

    新增設計BMI表單

--- index.php
+++ index.php
@@ -7,7 +7,20 @@
 </head>
 <body>
 <h1>計算BMI<h1>
+    <form action="result.php" method="get">
+        <div>
+        <label for="hegiht">身高:</label>
+        <input type="number" name="hegiht" id="height">
+        </div>
+        <div>
+        <label for="weight">體重: </label>
+        <input type="number" name="weight" id="weight">
+        </div>
+<div>
+<input type="submit" value="計算">
+<input type="reset" value="清空">
 
-    
+</div>    
+
 </body>
 </html>
