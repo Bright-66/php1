10d3369f1b33cfa04a48f25538abdeee091e4bb9
sunbody1024 <sunbody1024@gmail.com>
2024-10-30 13:05:02 +0800

    使用session 來記錄登入資料

--- login.php
+++ login.php
@@ -1,11 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>登入</title>
-
-    <!DOCTYPE html>
 <html lang="zh-Hant">
 <head>
     <meta charset="UTF-8">
@@ -57,7 +50,10 @@
     </style>
 </head>
 <body>
-
+<?php
+session_start();
+if(!isset($_SESSION['login'])){
+?>
     <div class="login-container">
         <h2>登入</h2>
         <form action="check_acc.php" method="post">
@@ -67,5 +63,14 @@
         </form>
     </div>
 
+    <?php
+}else{
+?>
+        <div>
+            你已登入
+        </div>
+<?php
+}
+?>
 </body>
 </html>
